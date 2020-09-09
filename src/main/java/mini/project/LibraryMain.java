7ee9b04bfb25ce0dd0d8cc5ddb08da1559095fb2
seunghee-ryu <shee.ryu88@gmail.com>
2020-09-09 20:59:40 +0900

    library

--- src/main/java/mini/project/LibraryMain.java
+++ src/main/java/mini/project/LibraryMain.java
@@ -1,15 +1,19 @@
 package mini.project;
 
+import mini.project.handler.LibraryHandler;
 import mini.project.handler.MemberHandler;
 import mini.project.util.PromptTest;
 
 // Member - 회원 정보 필드
 // MembrHandler - 회원 가입/목록/상세조회/수정/탈퇴/로그인
-public class Library {
+public class LibraryMain {
 
   public static void main(String[] args) {
 
+
     MemberHandler memberHandler = new MemberHandler();
+
+    LibraryHandler libraryHandler = new LibraryHandler();
 
     loop:
       while (true) {
@@ -22,6 +26,10 @@ public class Library {
           case "/member/update": memberHandler.update(); break;
           case "/member/delete": memberHandler.delete(); break;
           case "/member/login": memberHandler.login(); break;
+
+          case "/library/enter": libraryHandler.enter(); break;
+          case "/book/check": libraryHandler.checkBook(); break;
+          case "/book/info": libraryHandler.info(); break;
           case "quit":
           case "exit":
             System.out.println("종료됩니다!");
@@ -31,6 +39,6 @@ public class Library {
         }
         System.out.println(); // 이전 명령의 실행을 구분하기 위해 빈 줄 출력
       }
-  PromptTest.close();
+    PromptTest.close();
   }
 }
