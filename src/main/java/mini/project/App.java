d8b9111e10a319286e9963a5460e6964ec0de79b
j-jieun <jsje02@gmail.com>
2020-09-09 13:13:21 +0900

    bitcamp

--- src/main/java/mini/project/App.java
+++ src/main/java/mini/project/App.java
@@ -9,7 +9,9 @@ public class App {
   }
 
   public static void main(String[] args) {
-    System.out.println("20200909 지은");
+    System.out.println("안녕하세요.");
+    System.out.println("오호라...");
+
     System.out.println("bitcamp");
   }
 }
